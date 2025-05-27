8010219070dfac9564b78b4d2e9088b73f0c1400
Youness El Ouam <yel-ouam@c4r9p5.1337.ma>
2025-05-27 13:37:35 +0100

    minishell loop and history

---
+++ minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-ouam <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/27 13:24:51 by yel-ouam          #+#    #+#             */
+/*   Updated: 2025/05/27 13:27:21 by yel-ouam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <signal.h>
+
