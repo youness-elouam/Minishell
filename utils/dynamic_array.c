f4188d843969a5c8862ef1e2055cf3c2e6383349
Saad Loukili <saad-loukili@hotmail.com>
2025-06-26 23:00:32 +0100

    DA

--- utils/dynamic_array.c
+++ utils/dynamic_array.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dynamic_array.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yel-ouam <yel-ouam@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: sloukili <sloukili@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 20:46:44 by yel-ouam          #+#    #+#             */
-/*   Updated: 2025/06/26 22:46:22 by yel-ouam         ###   ########.fr       */
+/*   Updated: 2025/06/26 22:52:15 by sloukili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,20 +38,33 @@ size_t	ft_strlen(char *s)
 	return(i);
 }
 
-void	ft_strcpy(char *s1, char **s2)
+
+void	ft_strcpy(char *dest, char *src)
 {
 	int	i;
 
 	i = 0;
-	if (!s1)
+	if (!src)
 		return ;
-	*s2 = malloc((ft_strlen(s1) + 1) * sizeof(char));
-	while(s1[i])
+	while (src[i])
 	{
-		(*s2)[i] = s1[i];
+		dest[i] = src[i];
 		i++;
 	}
-	(*s2)[i] = '\0';
+	dest[i] = '\0';
+}
+
+char	*ft_strdup(char *s)
+{
+	size_t	len;
+	char	*rs;
+
+	len = ft_strlen(s);
+	rs = (char *)malloc((len + 1) * sizeof(char));
+	if (!rs)
+		return (NULL);
+	ft_strcpy(rs, s);
+	return (rs);
 }
 
 void	free_array(char **arr)
@@ -75,10 +88,10 @@ char	**dc_array(char **arr, char *s)
 	new = malloc(sizeof(char*) * (array_size(arr) + 2));
 	while (arr[i])
 	{
-		ft_strcpy(arr[i], &(new[i]));
+		new[i] = ft_strdup(arr[i]);
 		i++;
 	}
-	ft_strcpy(s, &(new[i++]));
+	new[i++] = ft_strdup(s);
 	new[i] = NULL;
 	free_array(arr);
 	return (new);
@@ -91,8 +104,8 @@ int	main()
 	
 	i = 0;
 	arr = malloc(sizeof(char *) * 3);
-	ft_strcpy("youness", &arr[0]);
-	ft_strcpy("Elouam", &arr[1]);
+	arr[0] = ft_strdup("youness");
+	arr[1] = ft_strdup("Elouam");
 	arr[2] = NULL;
 	while(arr[i])
 	{
